7abd18afcc3c08db6229b3ce7fa1779ca1552f94
Exterminate5573 <exterminate5573@gmail.com>
2021-11-22 15:59:10 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -45,8 +45,8 @@ addLayer("doge", {
 })
 
 addLayer("amogus", {
-    name: "Among Us",
-    symbol "A",
+    name: "Among Us", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     row: 1,
     startData() { return {
@@ -59,7 +59,7 @@ addLayer("amogus", {
       return req
     }, // Can be a function that takes requirement increases into account
     resource: "SusCoins", // Name of prestige currency
-    baseResource: "Sus", // Name of resource prestige is based on
+    baseResource: "Wow", // Name of resource prestige is based on
     baseAmount(){
       return player.points
     }, // Get the current amount of baseResource
@@ -68,18 +68,26 @@ addLayer("amogus", {
     upgrades: {
     rows: 2,
     cols: 5,
-    14: {
-      title: "Crewmate",
-      description: "Generate 10 memes per second",
+    11: {
+      title: "Wow",
+      description: "Generate 1 meme per second",
       cost: new Decimal(1),
-      unlocked() {return player.amogus.unlocked},
+      unlocked() {return player.doge.unlocked},
     },
-    15: {
-      title: "Imposter",
-      description: "Generate 15 memes per second",
+    12: {
+      title: "Very Wow",
+      description: "Generate 2 memes per second",
       cost: new Decimal(5),
-      unlocked() {return (hasUpgrade("amogus", 14))},
+      unlocked() {return (hasUpgrade("doge", 11))},
+    },
+    13: {
+      title: "Much Wow",
+      description: "Generate 7 memes per second",
+      cost: new Decimal(20),
+      unlocked() {return (hasUpgrade("doge", 12))},
     },
   },
   layerShown(){return true}
 })
+
+
