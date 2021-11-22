a9a6748cde16404dbcfbc3bb0b3f4456a679c554
Exterminate5573 <exterminate5573@gmail.com>
2021-11-22 19:29:06 +1100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -41,10 +41,10 @@ addLayer("doge", {
       unlocked() {return (hasUpgrade("doge", 12))},
     },
     14: {
-        title: "Concern",
-        description: "Generate 10 memes per second",
-        cost: new Decimal(40),
-        unlocked() {return (hasUpgrade("doge", 13))},
+      title: "Concern",
+      description: "Generate 10 memes per second",
+      cost: new Decimal(40),
+      unlocked() {return (hasUpgrade("doge", 13))},
     },
   },
   layerShown(){return true}
